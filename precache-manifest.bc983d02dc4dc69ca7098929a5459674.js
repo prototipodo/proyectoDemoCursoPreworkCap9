6f88f8fc783814d4179e5ede8550192848dec029
prototipodo <prototipodo@gmail.com>
2023-04-11 22:39:50 -0600

    Updates

---
+++ precache-manifest.bc983d02dc4dc69ca7098929a5459674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "988199c89355a70a0483",
+    "url": "/proyectoDemoCursoPreworkCap9/static/css/main.13cc386e.chunk.css"
+  },
+  {
+    "revision": "988199c89355a70a0483",
+    "url": "/proyectoDemoCursoPreworkCap9/static/js/main.988199c8.chunk.js"
+  },
+  {
+    "revision": "480a353721bd092f9455",
+    "url": "/proyectoDemoCursoPreworkCap9/static/js/1.480a3537.chunk.js"
+  },
+  {
+    "revision": "200caca44a155adc6d24",
+    "url": "/proyectoDemoCursoPreworkCap9/static/js/runtime~main.200caca4.js"
+  },
+  {
+    "revision": "69015fe75daa24af918dc05782a6e57e",
+    "url": "/proyectoDemoCursoPreworkCap9/index.html"
+  }
+];
